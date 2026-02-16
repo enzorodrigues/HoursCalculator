eb5004d8b0d29285f7db7d01be534519daef0202
Enzo Soares <enzorodrigues.2003@hotmail.com>
2026-02-16 12:09:11 -0300

    Initial Commit

---
+++ HoursCalculator/HoursCalculatorApp.swift
@@ -0,0 +1,17 @@
+//
+//  HoursCalculatorApp.swift
+//  HoursCalculator
+//
+//  Created by Enzo Soares on 16/02/26.
+//
+
+import SwiftUI
+
+@main
+struct HoursCalculatorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
